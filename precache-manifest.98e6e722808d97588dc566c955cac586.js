29d26858fc4687efe37b10262e08f92d7c6be0e1
Artur <grandfaraon93@gmail.com>
2019-12-09 13:51:16 +0200

    Updates

---
+++ precache-manifest.98e6e722808d97588dc566c955cac586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a871be77641211f119dd3400a52688d2",
+    "url": "/PriceList/index.html"
+  },
+  {
+    "revision": "9e3cb936dc36387e6e39",
+    "url": "/PriceList/static/css/main.8a2e8d98.chunk.css"
+  },
+  {
+    "revision": "7d499292f0fc8e26a196",
+    "url": "/PriceList/static/js/2.c3c800ab.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/PriceList/static/js/2.c3c800ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e3cb936dc36387e6e39",
+    "url": "/PriceList/static/js/main.09aad7b7.chunk.js"
+  },
+  {
+    "revision": "796ac117693393f91931",
+    "url": "/PriceList/static/js/runtime-main.fa1c18a9.js"
+  }
+]);
